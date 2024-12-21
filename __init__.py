02131dc5b20c1c5ed388c43fcd8ba23fe344128e
abdelrahman <bsyd654@gmaiol.com>
2024-12-21 14:22:26 +0200

    some Modify in __init__ file for the tast api

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 from flask import Flask , jsonify , Blueprint, request
 import routes as route
 app = Flask(__name__)
-app.register_blueprint(route.bp)
+app.register_blueprint(route.bp,url_prefix='/api')
 if __name__ == '__main__':
     app.run(debug=True)
